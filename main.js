4f53fde8df1984982ace3a4b0ee955ad07319a5d
SamBat2005 <batcsg1@student.op.ac.nz>
2024-08-23 17:08:52 +1200

    Implemented javascript for random fact box

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+//FACT-BOX
+//Declare fact array and values
+const fact1 = "Did you know it takes at least 30 to 40 years for Monkey Puzzles to produce male or female cones.";
+const fact2 = "The seeds/nuts of a Monkey Puzzle are actually edible, and have a nutty and milky flavour.";
+const fact3 = "Monkey Puzzles in their native range have been recorded to live for over 1000 years.";
+const fact4 = "In the native Chilean homeland of Monkey Puzzles, their wood was used to make salad bowls and salad tongs with.";
+const fact5 = "Although a Monkey Puzzle is either male of female (dioecious) there are very ocassionaly trees of both sexes (monoecious).";
+const facts = [fact1, fact2, fact3, fact4, fact5];
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+const para = document.getElementById('fact-box');
 
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display == "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+function displayFact(facts){
+  const random = Math.floor(Math.random()*facts.length);
+  para.textContent = facts[random];
 }
+//Call the display fact method
+displayFact(facts);
+
+
+
+
+
+
+
+
+

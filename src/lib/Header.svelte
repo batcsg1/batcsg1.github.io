17b96c641250ee006c2ba42aced17217f2703a41
SamBat2005 <batcsg1@student.op.ac.nz>
2024-10-16 20:10:26 +1300

    Added and linked Header component to main page

---
+++ src/lib/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<header>
+    <h1>Batchelor Monkey Puzzle Website</h1>
+    <nav>
+        <ul>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Contents</li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+
+
+</style>
